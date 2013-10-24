bf7e93fb68ffb4743521029c074b00c57ef901a3
Joris Vandeweerd <joris@statik.be>
2013-10-24 23:27:17 +0200

    initial commit

---
+++ src/pallo/web/template/ApplicationListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace pallo\web\template;
+
+use pallo\library\config\Config;
+use pallo\library\dependency\DependencyInjector;
+use pallo\library\event\Event;
+
+use pallo\web\mvc\view\TemplateView;
+
+/**
+ * Event listeners for the pallo-web-template module
+ */
+class ApplicationListener {
+
+    /**
+     * Event listener to inject the template engine in the template view of the
+     * response
+     * @param pallo\library\event\Event $event
+     * @return null
+     */
+    public function injectTemplateEngine(Event $event, Config $config, DependencyInjector $dependencyInjector) {
+        $web = $event->getArgument('web');
+        $response = $web->getResponse();
+        if (!$response) {
+            return;
+        }
+
+        $view = $response->getView();
+        if (!$view instanceof TemplateView || $view->getTemplateEngine()) {
+            return;
+        }
+
+        $templateEngineId = $config->get('template.engine');
+        $templateEngine = $dependencyInjector->get('pallo\\library\\template\\TemplateEngine', $templateEngineId);
+
+        $view->setTemplateEngine($templateEngine);
+    }
+
+}
